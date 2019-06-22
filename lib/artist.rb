53626fa11fd0d7276decb7f9783497213b033ee0
Marc Poppy <poppy.marc@gmail.com>
2019-06-22 22:21:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,8 +24,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if
-      self.all.map {|object| object.name == name}
+    if self.all.map {|object| object.name == name}
     else
      name = Artist.new(name)
     end
