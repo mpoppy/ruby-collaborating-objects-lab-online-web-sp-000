7aeba9828735a928afd1a4e65aa1f29a59d6b938
Marc Poppy <poppy.marc@gmail.com>
2019-06-22 21:26:15 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    # @@all << self
+  end
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(name)
+    #find first if doesn't exist then create a new instance of Artist and assign name
+    if !self.all.include?(name)
+        name = Artist.new(name)
+    else
+      self.name
+    end
+  end
+
+end
