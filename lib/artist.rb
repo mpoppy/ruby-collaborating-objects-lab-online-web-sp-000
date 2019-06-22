47af22e3dc64787d2c91614c120dda5a39846943
Marc Poppy <poppy.marc@gmail.com>
2019-06-22 22:00:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,7 +26,7 @@ class Artist
   def self.find_or_create_by_name(name)
     #find first if doesn't exist then create a new instance of Artist and assign name
     if self.all.include?(name)
-
+      
     else #if condition false
       name = Artist.new(name)
     end
