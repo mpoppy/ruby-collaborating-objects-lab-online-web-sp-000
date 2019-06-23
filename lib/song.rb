0021837d5497adfe7a16098a1e713d2de071ed5e
Marc Poppy <poppy.marc@gmail.com>
2019-06-23 00:10:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,9 @@ class Song
 
   def self.new_by_filename(file_name)
     song_name = file_name.split(" - ")[1]
-    song_artist = file_name.split(" - ")[0]
+    # song_artist = file_name.split(" - ")[0]
     song = self.new(song_name)
-    # self.artist = song_artist
+    song.artist = file_name.split(" - ")[0]
     # song.artist = Artist.new(song_artist)
   end
 
